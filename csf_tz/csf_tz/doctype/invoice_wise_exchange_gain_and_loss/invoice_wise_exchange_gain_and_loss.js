9d91e3c6e56cf93be3cdb576dda89eed842638ce
Info Aakvatech <info@aakvatech.com>
2019-02-25 09:57:32 +0000

    Removed unnecessary doctypes

--- csf_tz/csf_tz/doctype/invoice_wise_exchange_gain_and_loss/invoice_wise_exchange_gain_and_loss.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, Aakvatech and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Invoice wise Exchange Gain and Loss', {
-	refresh: function(frm) {
-
-	}
-});
