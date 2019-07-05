14034e3118dc1c112f7293849c723a678940e3f0
Info Aakvatech <info@aakvatech.com>
2019-07-05 13:53:39 +0000

    fix: Fixed double cost center and remove no remarks text

---
+++ csf_tz/csf_tz/report/itemwise_stock_movement/itemwise_stock_movement.js
@@ -0,0 +1,63 @@
+// Copyright (c) 2016, Aakvatech and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Itemwise Stock Movement"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
+		{
+			"fieldname":"warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Link",
+			"options": "Warehouse",
+			"default": "DAR - VC"
+		},
+		{
+			"fieldname":"item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"options": "Item Group",
+			"default": "VOUCHERS"
+		},
+		{
+			"fieldname":"brand",
+			"label": __("Brand"),
+			"fieldtype": "Link",
+			"options": "Brand",
+			"default": "HALOTEL"
+		},
+		{
+			"fieldname":"include_uom",
+			"label": __("Include UOM"),
+			"fieldtype": "Link",
+			"options": "UOM"
+		}
+	]
+}
+
+// $(function() {
+// 	$(wrapper).bind("show", function() {
+// 		frappe.query_report.load();
+// 	});
+// });
---
+++ csf_tz/csf_tz/report/itemwise_stock_movement/itemwise_stock_movement.py
@@ -0,0 +1,161 @@
+# Copyright (c) 2019, Aakvatech and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from erpnext.stock.utils import update_included_uom_in_report
+
+def execute(filters=None):
+	include_uom = filters.get("include_uom")
+	columns = get_columns()
+	items = get_items(filters)
+	sl_entries = get_stock_ledger_entries(filters, items)
+	item_details = get_item_details(items, sl_entries, include_uom)
+	# opening_row = get_item_balance(filters, columns, filters.from_date, "Opening", "Halotel 500")
+	# closing_row = get_item_balance(filters, columns, filters.to_date, "Closing", "Halotel 500")
+
+	data = []
+	conversion_factors = []
+	if opening_row:
+		data.append(opening_row)
+
+	for sle in sl_entries:
+		item_detail = item_details[sle.item_code]
+
+		sle.update(item_detail)
+		data.append(sle)
+
+		if include_uom:
+			conversion_factors.append(item_detail.conversion_factor)
+
+	update_included_uom_in_report(columns, data, include_uom, conversion_factors)
+
+	if closing_row:
+		data.append(closing_row)
+
+	return columns, data
+
+def get_columns():
+	columns = [
+		{"label": _("Date"), "fieldname": "date", "fieldtype": "Datetime", "width": 95},
+		{"label": _("Voucher Type"), "fieldname": "voucher_type", "width": 110},
+		{"label": _("Voucher #"), "fieldname": "voucher_no", "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 100},
+		{"label": _("Stock UOM"), "fieldname": "stock_uom", "fieldtype": "Link", "options": "UOM", "width": 100},
+		{"label": _("Qty"), "fieldname": "actual_qty", "fieldtype": "Float", "width": 50, "convertible": "qty"}
+	]
+
+	return columns
+
+def get_stock_ledger_entries(filters, items):
+	item_conditions_sql = ''
+	if items:
+		item_conditions_sql = 'and sle.item_code in ({})'\
+			.format(', '.join(['"' + frappe.db.escape(i) + '"' for i in items]))
+
+	return frappe.db.sql("""select concat_ws(" ", posting_date, posting_time) as date,
+			item_code, warehouse, actual_qty, qty_after_transaction, incoming_rate, valuation_rate,
+			stock_value, voucher_type, voucher_no, batch_no, serial_no, company, project
+		from `tabStock Ledger Entry` sle
+		where company = %(company)s and
+			posting_date between %(from_date)s and %(to_date)s
+			{sle_conditions}
+			{item_conditions_sql}
+			order by posting_date asc, posting_time asc, name asc"""\
+		.format(
+			sle_conditions=get_sle_conditions(filters),
+			item_conditions_sql = item_conditions_sql
+		), filters, as_dict=1)
+
+def get_items(filters):
+	conditions = []
+	if filters.get("item_code"):
+		conditions.append("item.name=%(item_code)s")
+	else:
+		if filters.get("brand"):
+			conditions.append("item.brand=%(brand)s")
+		if filters.get("item_group"):
+			conditions.append(get_item_group_condition(filters.get("item_group")))
+
+	items = []
+	if conditions:
+		items = frappe.db.sql_list("""select name from `tabItem` item where {}"""
+			.format(" and ".join(conditions)), filters)
+	return items
+
+def get_item_details(items, sl_entries, include_uom):
+	item_details = {}
+	if not items:
+		items = list(set([d.item_code for d in sl_entries]))
+
+	if not items:
+		return item_details
+
+	cf_field = cf_join = ""
+	if include_uom:
+		cf_field = ", ucd.conversion_factor"
+		cf_join = "left join `tabUOM Conversion Detail` ucd on ucd.parent=item.name and ucd.uom='%s'" \
+			% frappe.db.escape(include_uom)
+
+	item_codes = ', '.join(['"' + frappe.db.escape(i, percent=False) + '"' for i in items])
+	res = frappe.db.sql("""
+		select
+			item.name, item.item_name, item.description, item.item_group, item.brand, item.stock_uom {cf_field}
+		from
+			`tabItem` item
+			{cf_join}
+		where
+			item.name in ({item_codes})
+	""".format(cf_field=cf_field, cf_join=cf_join, item_codes=item_codes), as_dict=1)
+
+	for item in res:
+		item_details.setdefault(item.name, item)
+
+	return item_details
+
+def get_sle_conditions(filters):
+	conditions = []
+	if filters.get("warehouse"):
+		warehouse_condition = get_warehouse_condition(filters.get("warehouse"))
+		if warehouse_condition:
+			conditions.append(warehouse_condition)
+	if filters.get("voucher_no"):
+		conditions.append("voucher_no=%(voucher_no)s")
+
+	return "and {}".format(" and ".join(conditions)) if conditions else ""
+
+def get_opening_balance(filters, columns, date, balance_type, item_code):
+	if not (item_code and filters.warehouse and date):
+		return
+
+	from erpnext.stock.stock_ledger import get_previous_sle
+	last_entry = get_previous_sle({
+		"item_code": item_code,
+		"warehouse_condition": get_warehouse_condition(filters.warehouse),
+		"posting_date": date,
+		"posting_time": "00:00:00"
+	})
+	row = {}
+	row["voucher_type"] = _(balance_type)
+	for dummy, v in ((9, 'actual_qty')):
+			row[v] = last_entry.get(v, 0)
+
+	return row
+
+def get_warehouse_condition(warehouse):
+	warehouse_details = frappe.db.get_value("Warehouse", warehouse, ["lft", "rgt"], as_dict=1)
+	if warehouse_details:
+		return " exists (select name from `tabWarehouse` wh \
+			where wh.lft >= %s and wh.rgt <= %s and warehouse = wh.name)"%(warehouse_details.lft,
+			warehouse_details.rgt)
+
+	return ''
+
+def get_item_group_condition(item_group):
+	item_group_details = frappe.db.get_value("Item Group", item_group, ["lft", "rgt"], as_dict=1)
+	if item_group_details:
+		return "item.item_group in (select ig.name from `tabItem Group` ig \
+			where ig.lft >= %s and ig.rgt <= %s and item.item_group = ig.name)"%(item_group_details.lft,
+			item_group_details.rgt)
+
+	return ''
