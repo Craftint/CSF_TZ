60cfae15d955bc75b1890e0cb42eab644d62cde6
Info Aakvatech <info@aakvatech.com>
2019-09-27 06:38:16 +0000

    feat: Added new report Purchase Reports by Tax Category

---
+++ csf_tz/csf_tz/report/purchase_reports_by_tax_category/purchase_reports_by_tax_category.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2016, Aakvatech and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Purchase Reports by Tax Category"] = {
+        "filters": [
+			{
+				"fieldname":"from_date",
+				"label": __("From Date"),
+				"fieldtype": "Date",
+				"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+				"reqd": 1,
+				"width": "60px"
+			},
+			{
+				"fieldname":"to_date",
+				"label": __("To Date"),
+				"fieldtype": "Date",
+				"default": frappe.datetime.get_today(),
+				"reqd": 1,
+				"width": "60px"
+			},
+			{
+				"fieldname": "taxes_and_charges",
+				"label": __("Tax Type"),
+				"fieldtype": "Link",
+				"width": "80",
+				"options": "Purchase Taxes and Charges Template",
+				"default": ""
+			},
+        ]
+}
