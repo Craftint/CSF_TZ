dab3685d1e3d2831efd0d9ea6ba08d77d7aae03b
Info Aakvatech <info@aakvatech.com>
2019-09-27 08:52:10 +0000

    feat: Added report Purchases by Tax Category Summary

---
+++ csf_tz/csf_tz/report/purchases_by_tax_category_summary/purchases_by_tax_category_summary.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2016, Aakvatech and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Purchases by Tax Category Summary"] = {
+        "filters": [
+			{
+				"fieldname":"from_date",
+				"label": __("From Date"),
+				"fieldtype": "Date",
+				"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+				"reqd": 1,
+				"width": "60px"
+			},
+			{
+				"fieldname":"to_date",
+				"label": __("To Date"),
+				"fieldtype": "Date",
+				"default": frappe.datetime.get_today(),
+				"reqd": 1,
+				"width": "60px"
+			},
+        ]
+}
