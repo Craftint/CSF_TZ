d42395cd0c79d72df5f956b280ce7efbfc17ed93
Info Aakvatech <info@aakvatech.com>
2019-10-29 13:54:19 +0000

    fix: Update series no of sales invoice only if the document is draft

--- csf_tz/csf_tz/sales_invoice.js
+++ csf_tz/csf_tz/sales_invoice.js
@@ -1,10 +1,12 @@
 frappe.ui.form.on("Sales Invoice", {
     onload: function(frm) {
-        if (frm.doc.is_return == "0") {
-            frm.set_value("naming_series","ACC-SINV-.YYYY.-");
-        }
-        else if (frm.doc.is_return == "1") {
-            frm.set_value("naming_series","ACC-CN-.YYYY.-");
+        if (frm.doc.document_status == "Draft") {
+            if (frm.doc.is_return == "0") {
+                frm.set_value("naming_series","ACC-SINV-.YYYY.-");
+            }
+            else if (frm.doc.is_return == "1") {
+                frm.set_value("naming_series","ACC-CN-.YYYY.-");
+            }
         }
     },
 });
