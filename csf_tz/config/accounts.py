34292c6bb83f860dde67592bf572eccb4672bf88
Youssef Restom <youssefrestom@gmail.com>
2020-03-28 17:59:10 +0300

    feat: setup Multi-Currency Ledger report

---
+++ csf_tz/config/accounts.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+from frappe import _
+import frappe
+
+
+def get_data():
+	return [
+		
+		{
+			"label": _("General Ledger"),
+			"items": [
+				
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Multi-Currency Ledger",
+					"doctype": "GL Entry",
+					"description": _("Accounting journal entries with Multi-Currency."),
+				},
+			]
+		},
+	
+
+	]
