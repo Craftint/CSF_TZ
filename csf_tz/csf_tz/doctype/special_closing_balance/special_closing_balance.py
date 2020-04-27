df77c1cdfe29ba93e8727b20a9a2dcf0276c6340
Info Aakvatech <info@aakvatech.com>
2020-04-27 06:57:02 +0000

    fix: Deepsource suggestion to remove spaces around parameter equals

--- csf_tz/csf_tz/doctype/special_closing_balance/special_closing_balance.py
+++ csf_tz/csf_tz/doctype/special_closing_balance/special_closing_balance.py
@@ -15,27 +15,27 @@ class SpecialClosingBalance(Document):
 		user_remarks = "Special Closing Balance - {0}".format(self.name)
 		for item_row in self.closing_balance_details:
 			if item_row.item and item_row.quantity:
-				item_balance = get_latest_stock_qty(item_row.item,self.warehouse)
+				item_balance = get_latest_stock_qty(item_row.item, self.warehouse)
 				item_dict = dict(
-					item_code = item_row.item,
-					qty = item_row.quantity - item_balance,
-					uom = item_row.uom,
-					s_warehouse = self.warehouse
+					item_code=item_row.item,
+					qty=item_row.quantity - item_balance,
+					uom=item_row.uom,
+					s_warehouse=self.warehouse
 				)
 				item_row.item_balance = item_balance
 				items.append(item_dict)
 
-		stock_entry_doc =frappe.get_doc(dict(
-				doctype = "Stock Entry",
-				posting_date= today(),
-				items = items,
+		stock_entry_doc = frappe.get_doc(dict(
+				doctype="Stock Entry",
+				posting_date=today(),
+				items=items,
 				stock_entry_type='Material Receipt',
 				purpose='Material Receipt',
-				to_warehouse = self.warehouse,
-				company= self.company, 
-				remarks = user_remarks,
-				special_closing_balance = self.name
-				)).insert(ignore_permissions = True)
+				to_warehouse=self.warehouse,
+				company=self.company, 
+				remarks=user_remarks,
+				special_closing_balance=self.name
+				)).insert(ignore_permissions=True)
 		if stock_entry_doc:
 			frappe.flags.ignore_account_permission = True
 			stock_entry_doc.submit()
