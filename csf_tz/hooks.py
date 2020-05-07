45c36e4ae9c782354eaf55ba246bf9b4606c30a7
Youssef Restom <youssefrestom@gmail.com>
2020-05-07 20:54:31 +0300

    feat: update hooks to Company-max_records_in_dialog

--- csf_tz/hooks.py
+++ csf_tz/hooks.py
@@ -35,7 +35,8 @@ fixtures = [
 		"Sales Invoice-electronic_fiscal_device",
 		"Sales Invoice-efd_z_report",
 		"POS Profile-column_break_1",
-		"POS Profile-electronic_fiscal_device"
+		"POS Profile-electronic_fiscal_device",
+		"Company-max_records_in_dialog",
 	)]]},
 	{"doctype":"Property Setter", "filters": [["name", "in", (
 		"Sales Invoice-default_print_format",
@@ -57,7 +58,7 @@ fixtures = [
 		"Payment Entry-section_break_12-collapsible",
 		"Payment Entry-payment_accounts_section-collapsible",
 		"Special Closing Balance-naming_series-options",
-		"Special Closing Balance-naming_series-default"
+		"Special Closing Balance-naming_series-default",
 	)]]},
 ]
 
