a5cc717abd690ee71c941f212911b8864f7605f1
Aakvatech Limited <info@aakvatech.com>
2020-06-25 07:26:45 +0000

    Update warehouse.js

--- csf_tz/csf_tz/warehouse.js
+++ csf_tz/csf_tz/warehouse.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("Warehouse", {
 	
 	refresh: function(frm, dt, dn) {
 		
-        frm.add_custom_button(__('Creat Stock Reconciliation'),
+        frm.add_custom_button(__('Create Stock Reconciliation'),
             function() {
                 frappe.call({
                     method: 'csf_tz.custom_api.make_stock_reconciliation_for_all_pending_material_request',
