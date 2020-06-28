909b8888b20e0e575130e7de0315f7c22f9fa306
Youssef Restom <youssefrestom@gmail.com>
2020-06-28 20:52:54 +0000

    fix: delivery status for non maintan items

--- csf_tz/custom_api.py
+++ csf_tz/custom_api.py
@@ -622,57 +622,68 @@ def validate_items_remaining_qty(doc, methohd):
 
 
 def check_validate_delivery_note(doc=None, method=None, doc_name=None):
-    if not doc and doc_name:
-        doc = frappe.get_doc("Sales Invoice", doc_name)
-        doc.to_save = True
-    else:
-        doc.to_save = False
-    doc.db_set('delivery_status', "Not Delivered", commit=True)
-    if doc.update_stock:
-        return
-    
-    part_delivery = False
-    # full_delivery = False
-    items_qty = 0
-    items_delivered_qty = 0
-    i = 0
-    for item in doc.items:
-        if doc.is_new():
-            item.db_set('delivery_status', "Not Delivered", commit=True)
-            item.delivered_qty = 0
-        items_qty += item.stock_qty
-        if item.delivery_note or item.delivered_by_supplier:
-            part_delivery = True
-            i += 1
-        if item.delivered_qty:
-            if item.stock_qty == item.delivered_qty:
-                item.db_set('delivery_status', "Delivered", commit=True)
-            elif item.stock_qty < item.delivered_qty:
-                item.db_set('delivery_status', "Over Delivered", commit=True)
-            elif item.stock_qty > item.delivered_qty and item.delivered_qty > 0:
-                item.db_set('delivery_status', "Part Delivery", commit=True)
-            items_delivered_qty += item.delivered_qty
-    if i == len(doc.items):
-        doc.db_set('delivery_status', "Delivered", commit=True)
-    elif doc.to_save and items_delivered_qty >= items_qty:
-        doc.db_set('delivery_status', "Delivered", commit=True)
-    elif doc.to_save and items_delivered_qty <= items_qty and items_delivered_qty > 0:
-        doc.db_set('delivery_status', "Part Delivered", commit=True)
-    elif part_delivery:
-        doc.db_set('delivery_status', "Part Delivery", commit=True)
-    else:
-        doc.db_set('delivery_status', "Not Delivered", commit=True)
-    if doc.to_save:
-        doc.save()
-        
+	if not doc and doc_name:
+		doc = frappe.get_doc("Sales Invoice", doc_name)
+		doc.to_save = True
+	else:
+		doc.to_save = False
+	doc.delivery_status = "Not Delivered"
+	if doc.update_stock:
+		return
+
+	part_delivery = False
+	# full_delivery = False
+	items_qty = 0
+	items_delivered_qty = 0
+	i = 0
+	for item in doc.items:
+		if doc.is_new():
+			item.delivery_status = "Not Delivered"
+			item.delivered_qty = 0
+		items_qty += item.stock_qty
+		if item.delivery_note or item.delivered_by_supplier:
+			part_delivery = True
+			i += 1
+		if item.delivered_qty:
+			if item.stock_qty == item.delivered_qty:
+				item.delivery_status = "Delivered"
+			elif item.stock_qty < item.delivered_qty:
+				item.delivery_status = "Over Delivered"
+			elif item.stock_qty > item.delivered_qty and item.delivered_qty > 0:
+				item.delivery_status = "Part Delivered"
+			items_delivered_qty += item.delivered_qty
+	if i == len(doc.items):
+		doc.delivery_status = "Delivered"
+	elif doc.to_save and items_delivered_qty >= items_qty:
+		doc.delivery_status = "Delivered"
+	elif doc.to_save and items_delivered_qty <= items_qty and items_delivered_qty > 0:
+		doc.delivery_status = "Part Delivered"
+	elif part_delivery:
+		doc.delivery_status = "Part Delivered"
+	else:
+		doc.delivery_status = "Not Delivered"
+	if doc.to_save:
+		doc.flags.ignore_permissions = True
+		doc.save()
+
 
 
 def check_submit_delivery_note(doc, method):
-    if doc.update_stock:
-        doc.db_set('delivery_status', "Delivered", commit=True)
-        for item in doc.items:
-            item.db_set('delivered_qty', item.stock_qty, commit=True)
-            item.db_set('delivery_status', "Delivered", commit=True)
+	if doc.update_stock:
+		doc.db_set('delivery_status', "Delivered", commit=True)
+		for item in doc.items:
+			item.db_set('delivered_qty', item.stock_qty, commit=True)
+			item.db_set('delivery_status', "Delivered", commit=True)
+	else :
+		part_deivery = False
+		for item in doc.items:
+			if not check_item_is_maintain(item.item_code):
+				item.db_set('delivered_qty', item.stock_qty, commit=True)
+				item.db_set('delivery_status', "Delivered", commit=True)
+				part_deivery = True
+		if part_deivery:
+			doc.db_set('delivery_status', "Part Delivered", commit=True)
+			
 
 
 
