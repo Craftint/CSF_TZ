9cdad76ef1ca496b3990ef5505a9096accbb750c
Info Aakvatech <info@aakvatech.com>
2020-07-03 06:41:22 +0000

    fix: naming series updated even after initial save

--- csf_tz/csf_tz/payment_entry.js
+++ csf_tz/csf_tz/payment_entry.js
@@ -1,9 +1,11 @@
 frappe.ui.form.on("Payment Entry", {
 	onload: function(frm) {
-		frm.trigger("payment_type");
+		if ((frm.is_new())) {
+			frm.trigger("payment_type");
+		}
 	},
 	payment_type: function(frm) {
-        if (frm.doc.document_status == "Draft" || typeof frm.doc.document_status === "undefined") {
+        if (typeof frm.doc.document_status === "undefined") {
             if (frm.doc.payment_type == "Receive") {
                 frm.set_value("naming_series","RE-.YYYY.-");
                 frm.set_value("party_type", "Customer");
