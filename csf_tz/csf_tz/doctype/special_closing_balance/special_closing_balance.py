ff22a42b7325e99ba32ddb0107500bf2a66ce27a
Youssef Restom <youssefrestom@gmail.com>
2020-07-04 12:48:33 +0000

    feat: Fetch from Warehouse in Special Closing Balance

--- csf_tz/csf_tz/doctype/special_closing_balance/special_closing_balance.py
+++ csf_tz/csf_tz/doctype/special_closing_balance/special_closing_balance.py
@@ -7,7 +7,7 @@ import frappe
 # from frappe import _
 from frappe.utils import today
 from frappe.model.document import Document
-from erpnext.stock.utils import get_latest_stock_qty
+from erpnext.stock.utils import get_latest_stock_qty, get_stock_balance
 
 
 class SpecialClosingBalance(Document):
@@ -46,3 +46,43 @@ class SpecialClosingBalance(Document):
 			self.stock_entry = stock_entry_doc.name
 			url = frappe.utils.get_url_to_form(stock_entry_doc.doctype, stock_entry_doc.name)
 			frappe.msgprint("Stock Entry Created <a href='{0}'>{1}</a>".format(url,stock_entry_doc.name))
+
+@frappe.whitelist()
+def get_items(warehouse, posting_date, posting_time, company):
+	lft, rgt = frappe.db.get_value("Warehouse", warehouse, ["lft", "rgt"])
+	items = frappe.db.sql("""
+		select i.name, i.item_name, i.stock_uom, bin.warehouse
+		from tabBin bin, tabItem i
+		where i.name=bin.item_code and i.disabled=0 and i.is_stock_item = 1
+		and i.has_variants = 0 and i.has_serial_no = 0 and i.has_batch_no = 0
+		and exists(select name from `tabWarehouse` where lft >= %s and rgt <= %s and name=bin.warehouse)
+	""", (lft, rgt))
+
+	items += frappe.db.sql("""
+		select i.name, i.item_name, i.stock_uom, id.default_warehouse
+		from tabItem i, `tabItem Default` id
+		where i.name = id.parent
+			and exists(select name from `tabWarehouse` where lft >= %s and rgt <= %s and name=id.default_warehouse)
+			and i.is_stock_item = 1 and i.has_serial_no = 0 and i.has_batch_no = 0
+			and i.has_variants = 0 and i.disabled = 0 and id.company=%s
+		group by i.name
+	""", (lft, rgt, company))
+
+	res = []
+	for d in set(items):
+		stock_bal = get_stock_balance(d[0], d[3], posting_date, posting_time,
+			with_valuation_rate=True)
+
+		if frappe.db.get_value("Item", d[0], "disabled") == 0:
+			res.append({
+				"item": d[0],
+				"uom": d[2],
+				"warehouse": d[3],
+				"quantity": stock_bal[0],
+				"item_name": d[1],
+				"valuation_rate": stock_bal[1],
+				"item_balance": stock_bal[0],
+				"current_valuation_rate": stock_bal[1]
+			})
+
+	return res
