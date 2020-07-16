dde466e974c25a9780e4c005d6848d75e407e9fd
Info Aakvatech <info@aakvatech.com>
2020-07-16 07:05:02 +0000

    fix: Removed duplicate doctypes in custom app that exists in ERPNext core

--- csf_tz/workshop/doctype/job_card/job_card_list.js
+++
@@ -1,9 +0,0 @@
-frappe.listview_settings['Job Card'] = {
-	get_indicator: function(doc) {
-		if(doc.status=="Open") {
-			return [__("Open"), "orange", "status,=,Open"];
-		} else if(doc.status=="Closed") {
-			return [__("Closed"), "green", "status,=,Open"];
-		}
-	}
-};
