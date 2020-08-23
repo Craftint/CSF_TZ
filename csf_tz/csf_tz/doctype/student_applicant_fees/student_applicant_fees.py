5bb24e473fc6097fd81ac9d89a156cdd9e9d2e05
Youssef Restom <youssefrestom@gmail.com>
2020-08-23 01:10:54 +0000

    feat: send Student Applicant Fees to NMB Bank

--- csf_tz/csf_tz/doctype/student_applicant_fees/student_applicant_fees.py
+++ csf_tz/csf_tz/doctype/student_applicant_fees/student_applicant_fees.py
@@ -3,8 +3,24 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
+import binascii
+import os
+from csf_tz.bank_api import invoice_submission
 
 class StudentApplicantFees(Document):
-	pass
+	def after_insert(self):
+		self.callback_token = binascii.hexlify(os.urandom(14)).decode()
+		series = frappe.get_value("Company", self.company, "nmb_series") or ""
+		if not series:
+			frappe.throw(_("Please set NMB User Series in Company {0}".format(self.company)))
+		reference = str(series) + str(self.name)
+		if not self.abbr:
+			self.abbr = frappe.get_value("Company", self.company, "abbr") or ""
+		self.bank_reference = reference.replace('-', '').replace('FEE'+self.abbr,'')
+
+
+	def on_submit(self):
+		invoice_submission(self)
