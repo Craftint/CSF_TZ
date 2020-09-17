0c6c2dc39ef97b41f44784292f0174112e4ddc57
Info Aakvatech <info@aakvatech.com>
2020-09-17 07:02:57 +0000

    chore: bumped upto 1.5.06

--- csf_tz/__init__.py
+++ csf_tz/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 import frappe
 
-__version__ = '1.5.03'
+__version__ = '1.5.04'
 
 def console(*data):
 	frappe.publish_realtime('out_to_console', data, user=frappe.session.user)
