c9656865c23b0ab20f751107feabde939fb7812b
Youssef Restom <youssefrestom@gmail.com>
2020-11-14 16:06:23 +0000

    feat: Opening Balance as per General Ledger & Closing Balance as per General Ledger in Bank Clearance Pro

--- csf_tz/csf_tz/doctype/bank_clearance_pro/bank_clearance_pro.js
+++ csf_tz/csf_tz/doctype/bank_clearance_pro/bank_clearance_pro.js
@@ -40,6 +40,23 @@ frappe.ui.form.on('Bank Clearance Pro', {
 		});
 	},
 	get_payment_entries: function(frm) {
+		frappe.call({
+            method: 'erpnext.accounts.utils.get_balance_on',
+            args: {
+                account: frm.doc.account,
+                date: frappe.datetime.add_days(frm.doc.from_date, -1),
+            },
+            async: false,
+            callback: function (r) {
+                if (r.message) {
+                    frm.set_value("opening_balance", r.message || 0);
+                }
+                else {
+                    frm.set_value("opening_balance", 0);
+                }
+            }
+        });
+        frm.set_value("closing_balance", frm.doc.total_amount + frm.doc.opening_balance);
 		return frappe.call({
 			method: "get_payment_entries",
 			doc: frm.doc,
@@ -54,5 +71,5 @@ frappe.ui.form.on('Bank Clearance Pro', {
 				})
 			}
 		});
-	}
+	},
 });
