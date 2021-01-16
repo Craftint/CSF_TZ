bef1f283146fc04f0a1339ac3bdff367ce52b682
Info Aakvatech <info@aakvatech.com>
2021-01-16 21:22:53 +0000

    feat: Piecework DocTypes

--- csf_tz/__init__.py
+++ csf_tz/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 import frappe
 
-__version__ = '1.7.2'
+__version__ = '1.7.3'
 
 def console(*data):
 	frappe.publish_realtime('out_to_console', data, user=frappe.session.user)
---
+++ csf_tz/csf_tz/doctype/piecework_payment_allocation/piecework_payment_allocation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Piecework Payment Allocation', {
+	// refresh: function(frm) {
+
+	// }
+});
