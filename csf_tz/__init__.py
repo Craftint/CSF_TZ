f62df54b6850d1e4167ee804df615ec765e8acfc
Youssef Restom <youssefrestom@gmail.com>
2021-01-17 00:41:42 +0000

    feat: init Piecework

--- csf_tz/__init__.py
+++ csf_tz/__init__.py
@@ -2,11 +2,8 @@
 from __future__ import unicode_literals
 import frappe
 
-<<<<<<< HEAD
 __version__ = '1.7.3'
-=======
-__version__ = '1.7.2'
->>>>>>> a221704909c9ffb67abd68a6982d694198409c1d
+
 
 def console(*data):
-	frappe.publish_realtime('out_to_console', data, user=frappe.session.user)
+    frappe.publish_realtime('out_to_console', data, user=frappe.session.user)
