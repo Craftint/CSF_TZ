6974dac6e787d8e2214ac80059442673b6736c60
Info Aakvatech <info@aakvatech.com>
2021-03-13 14:10:45 +0000

    feat: Allow selection of default warehouse with names like Parts Store and fix name of child table from vehicle_consignment to work_order_consignment

--- csf_tz/csf_tz/doctype/work_order_consignment/work_order_consignment.js
+++ csf_tz/csf_tz/doctype/work_order_consignment/work_order_consignment.js
@@ -2,6 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Work Order Consignment', {
+    onload: function (frm) {
+        frm.set_query("default_source_warehouse", function () {
+            return { filters: [["name", "like", "%Parts Store%"]] };
+        });
+    },
     parent_item: function (frm) {
         if (frm.doc.parent_item) {
             frappe.call({
@@ -10,15 +15,15 @@ frappe.ui.form.on('Work Order Consignment', {
                     "item": frm.doc.parent_item,
                 },
                 callback: function (data) {
-                    frm.clear_table("vehicle_consignment_detail");
+                    frm.clear_table("work_order_consignment_detail");
                     if (data.message) {
                         data.message.forEach(element => {
-                            var child = frm.add_child("vehicle_consignment_detail");
+                            var child = frm.add_child("work_order_consignment_detail");
                             frappe.model.set_value(child.doctype, child.name, "bom", element.name);
 
                         });
                     }
-                    frm.refresh_field("vehicle_consignment_detail");
+                    frm.refresh_field("work_order_consignment_detail");
                 }
             });
         }
