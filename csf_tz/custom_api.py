5fa2b5a6446ac93cfa680da58058c66142749f48
Info Aakvatech <info@aakvatech.com>
2021-04-18 09:16:57 +0000

    fix: VFD QRCode to ignore html tags

--- csf_tz/custom_api.py
+++ csf_tz/custom_api.py
@@ -22,7 +22,7 @@ def generate_qrcode(qrcode_data):
     c = pyqrcode.create(qrcode_data)
     s = io.BytesIO()
     c.png(s,scale=3)
-    encoded = '<img src="data:image/png;base64,'+ base64.b64encode(s.getvalue()).decode("ASCII") + '">'
+    encoded = 'data:image/png;base64,'+ base64.b64encode(s.getvalue()).decode("ASCII")
     return encoded
 
 @frappe.whitelist()
