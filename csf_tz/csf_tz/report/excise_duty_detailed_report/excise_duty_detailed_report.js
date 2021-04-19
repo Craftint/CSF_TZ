141839ee7f65c86ec7baed1deca23db6e08d30a3
Info Aakvatech <info@aakvatech.com>
2021-04-19 09:13:24 +0000

    fix excise duty report for v12 compatibility

---
+++ csf_tz/csf_tz/report/excise_duty_detailed_report/excise_duty_detailed_report.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2016, Aakvatech and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Excise Duty Detailed Report"] = {
+    "filters": [
+        {
+         "fieldname": "from_date",
+         "fieldtype": "Date",
+         "label": "From Date",
+         "mandatory": 1,
+         "wildcard_filter": 0
+        },
+        {
+         "fieldname": "to_date",
+         "fieldtype": "Date",
+         "label": "To Date",
+         "mandatory": 1,
+         "wildcard_filter": 0
+        }
+    ]
+}
