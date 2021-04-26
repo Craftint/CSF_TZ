84349e2891d112dfb29fe96a4d897b75393f796f
Youssef Restom <youssefrestom@gmail.com>
2021-04-26 21:14:19 +0000

    feat: Piecework Single functionality

--- csf_tz/csf_tz/doctype/piecework_single/piecework_single.js
+++ csf_tz/csf_tz/doctype/piecework_single/piecework_single.js
@@ -6,3 +6,23 @@ frappe.ui.form.on('Piecework Single', {
 
 	// }
 });
+
+frappe.ui.form.on('Single Piecework Employees', {
+	task: (frm, cdt, cdn) => {
+		const row = locals[cdt][cdn];
+		setTotal(frm, row);
+	},
+	task_rate: (frm, cdt, cdn) => {
+		const row = locals[cdt][cdn];
+		setTotal(frm, row);
+	},
+	quantity: (frm, cdt, cdn) => {
+		const row = locals[cdt][cdn];
+		setTotal(frm, row);
+	},
+});
+
+const setTotal = (frm, row) => {
+	const total = row.task_rate * row.quantity || 0;
+	frappe.model.set_value(row.doctype, row.name, "amount", total);
+};
