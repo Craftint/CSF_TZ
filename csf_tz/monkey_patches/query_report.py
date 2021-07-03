536d16e86a1494804b85fdbcdc33e4243dcdffc3
Aakvatech Limited <info@aakvatech.com>
2021-07-03 22:49:29 +0200

    feat: Monkey patch to allow use of AV Report Extension

---
+++ csf_tz/monkey_patches/query_report.py
@@ -0,0 +1,23 @@
+import frappe
+
+from frappe.desk import query_report
+
+# override print formats
+old_get_script = query_report.get_script
+
+
+@frappe.whitelist()
+def get_script(report_name):
+    old_result = old_get_script(report_name)
+
+    av_report_extension_doc = frappe.get_doc("AV Report Extension", report_name)
+    if av_report_extension_doc:
+        if av_report_extension_doc.script:
+            old_result["script"] = av_report_extension_doc.script
+        if av_report_extension_doc.html_format:
+            old_result["html_format"] = av_report_extension_doc.html_format
+
+    return old_result
+
+
+query_report.get_script = get_script
