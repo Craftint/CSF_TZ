83f56d7c4ece5a3285adb65c2c04396dedd399a5
Aakvatech Limited <info@aakvatech.com>
2021-07-04 21:19:56 +0200

    feat: AV Report Extension active checkbox and check if exists to avoid unnecessary errors

--- csf_tz/monkey_patches/query_report.py
+++ csf_tz/monkey_patches/query_report.py
@@ -10,8 +10,10 @@ old_get_script = query_report.get_script
 def get_script(report_name):
     old_result = old_get_script(report_name)
 
+    if not frappe.db.exists("AV Report Extension", report_name):
+        return old_result
     av_report_extension_doc = frappe.get_doc("AV Report Extension", report_name)
-    if av_report_extension_doc:
+    if av_report_extension_doc.active:
         if av_report_extension_doc.script:
             old_result["script"] = av_report_extension_doc.script
         if av_report_extension_doc.html_format:
