b3f935230a9b9c4ec8575ab180999b27418d8595
Youssef <youssefrestom@gmail.com>
2021-09-18 20:50:04 +0000

    feat: Parking Bil Doctype

---
+++ csf_tz/csf_tz/doctype/parking_bil/parking_bil.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Parking Bil', {
+	// refresh: function(frm) {
+
+	// }
+});
