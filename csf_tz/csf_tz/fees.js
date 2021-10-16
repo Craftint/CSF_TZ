5cf684ba9e4c1d2958905113980cb54ebc3e9387
Aakvatech Limited <info@aakvatech.com>
2021-10-16 10:04:13 +0200

    feat: Set sales invoice income account filter

--- csf_tz/csf_tz/fees.js
+++ csf_tz/csf_tz/fees.js
@@ -1,19 +1,26 @@
 frappe.ui.form.on('Fees', {
-	refresh: function(frm){
+    refresh: function (frm) {
         if (frm.doc.docstatus == 1 && frm.doc.outstanding_amount > 0) {
-            frm.add_custom_button(__("Invoice Submission"), function(){
+            frm.add_custom_button(__("Invoice Submission"), function () {
                 frappe.call({
                     method: 'csf_tz.bank_api.invoice_submission',
                     args: {
                         fees_name: frm.doc.name,
                     },
-                    callback: function(r) {
+                    callback: function (r) {
                         if (r.message) {
-                           console.log(r.message);
+                            console.log(r.message);
                         }
                     }
                 });
-              });
-        }
+            });
+        };
+        frm.set_query("sales_invoice_income_account", function () {
+            return {
+                filters: [
+                    ["Account", "company", "=", frm.doc.company]
+                ]
+            };
+        });
     },
 });
