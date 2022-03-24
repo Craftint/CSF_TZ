3f11422926c0c271e6d552d82d53068c1a236fd9
Sagar Vora <sagar@resilient.tech>
2022-03-24 23:13:47 +0530

    fix: temp patch for item rate issue

--- csf_tz/__init__.py
+++ csf_tz/__init__.py
@@ -6,57 +6,55 @@ import importlib
 import frappe
 
 __version__ = "2.1.0"
+
 patches_loaded = False
+app_name = "csf_tz"
+
+
+def load_monkey_patches():
+    """
+    Loads all modules present in monkey_patches to override some logic
+    in Frappe / ERPNext. Returns if patches have already been loaded earlier.
+    """
+    global patches_loaded
+
+    if patches_loaded or app_name not in frappe.get_installed_apps():
+        return
+
+    patches_loaded = True
+
+    for module_name in os.listdir(frappe.get_app_path(app_name, "monkey_patches")):
+        if not module_name.endswith(".py") or module_name == "__init__.py":
+            continue
+
+        importlib.import_module(app_name + ".monkey_patches." + module_name[:-3])
+
+
+old_get_hooks = frappe.get_hooks
+
+
+def get_hooks(*args, **kwargs):
+    load_monkey_patches()
+    return old_get_hooks(*args, **kwargs)
+
+
+frappe.get_hooks = get_hooks
+
+old_connect = frappe.connect
+
+
+def connect(*args, **kwargs):
+    """
+    Patches frappe.connect to load monkey patches once a connection is
+    established with the database.
+    """
+
+    old_connect(*args, **kwargs)
+    load_monkey_patches()
+
+
+frappe.connect = connect
 
 
 def console(*data):
     frappe.publish_realtime("out_to_console", data, user=frappe.session.user)
-
-
-def load_monkey_patches():
-    global patches_loaded
-
-    if (
-        patches_loaded
-        or not getattr(frappe, "conf", None)
-        or not "csf_tz" in frappe.get_installed_apps()
-    ):
-        return
-
-    for module_name in os.listdir(frappe.get_app_path("csf_tz", "monkey_patches")):
-        if not module_name.endswith(".py") or module_name == "__init__.py":
-            continue
-
-        importlib.import_module("csf_tz.monkey_patches." + module_name[:-3])
-
-    patches_loaded = True
-
-
-
-connect = frappe.connect
-
-
-def custom_connect(*args, **kwargs):
-    out = connect(*args, **kwargs)
-
-    if frappe.conf.auto_commit_on_many_writes:
-        frappe.db.auto_commit_on_many_writes = 1
-
-    load_monkey_patches()
-    return out
-
-
-frappe.connect = custom_connect
-
-
-# # For Web Patches
-
-# get_hooks = frappe.get_hooks
-
-
-# def custom_get_hooks(*args, **kwargs):
-#     load_monkey_patches()
-#     return get_hooks(*args, **kwargs)
-
-
-# frappe.get_hooks = custom_get_hooks
