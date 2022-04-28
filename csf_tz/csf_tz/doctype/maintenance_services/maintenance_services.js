b5cb2d59d8f4e958ad7d4cb89421cfbec992abc5
Craftint <shajeer@craftinteractive.ae>
2022-04-28 13:45:01 +0400

    bug fix

---
+++ csf_tz/csf_tz/doctype/maintenance_services/maintenance_services.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Maintenance Services', {
+	// refresh: function(frm) {
+
+	// }
+});
